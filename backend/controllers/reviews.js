f1fb627f0bf12535c1633318a87284e2cb4573b7
skysalisbury <skysalisbury@gmail.com>
2025-06-09 17:14:42 -0400

    Review routes should be good, refactor/rewrite review controller on going

--- backend/controllers/reviews.js
+++ backend/controllers/reviews.js
@@ -1,60 +1,90 @@
-// const Review = require('../models/review');
+const Review = require('../models/review');
+const Game = require('../models/game');
 
-// module.exports = {
-//   index,
-//   show,
-//   create,
-//   update,
-//   deleteReview,
-// };
+module.exports = {
+  index,
+  show,
+  create,
+  update,
+  deleteReview,
+};
 
-// async function index(req, res) {
-//   try {
-//     const reviews = await Review.find({}).populate('author').populate('game');
-//     // Below would return all posts for just the logged in user
-//     // const posts = await Post.find({author: req.user._id});
-//     res.json(reviews);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ message: 'Failed to fetch Review' });
-//   }
-// }
+async function index(req, res) {
+  try {
+    const reviews = await Review.find({}).populate('author').populate('game');
+    // Below would return all posts for just the logged in user
+    // const posts = await Post.find({author: req.user._id});
+    res.json(reviews);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Failed to fetch Review' });
+  }
+}
 
-// async function create(req, res) {
-//   try {
-//     req.body.author = req.user._id;
-//     const review = await Review.create(req.body);
-//     res.json(review);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(400).json({ message: 'Failed to fetch Review' });
-//   }
-// }
+async function create(req, res) {
+  try {
+    const game = await Game.findById(req.params.gameId);
+    if (!game) return res.status(404).json({ message: 'Game not found' });
 
-// async function show(req, res) {
-//   try {
-//     const reviews = await Review.findById(req.params.reviewId)
-//       .populate('author')
-//       .populate('game');
-//     // Below would return all posts for just the logged in user
-//     // const posts = await Post.find({author: req.user._id});
-//     res.json(reviews);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(404).json({ message: 'Review not found' });
-//   }
-// }
+    const review = await Review.create({
+      text: req.body.text,
+      rating: req.body.rating,
+      author: req.user._id,
+      game: game._id,
+    });
 
-// async function update(req, res) {
-//   try {
-//     const review = await Review.findById(req.params.reviewId);
-//     if (!review.author.equals(req.user._id)) {
-//       return res.status(403).json({ message: 'Not authorized' });
-//     }
-//     Object.assign(review, req.body);
-//     await review.save();
-//     res.json(review);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// }
+    game.reviews.push(review._id);
+    await game.save();
+
+    await review.populate('author');
+    res.status(201).json(Review);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+}
+
+async function show(req, res) {
+  try {
+    const game = await Game.findById(req.params.gameId).populate(
+      'comments.author'
+    );
+    const review = game.reviews.id(req.params.reviewId); // correct way to access subdoc
+
+    if (!review) return res.status(404).json({ message: 'Review not found' });
+
+    res.json(review);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Failed to fetch review' });
+  }
+}
+
+async function update(req, res) {
+  try {
+    const game = await Game.findById(req.params.gameId);
+    const review = game.reviews.id(req.params.reviewId);
+
+    if (!review) return res.status(404).json({ message: 'Comment not found' });
+
+    if (!review.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to update this review");
+    }
+
+    review.text = req.body.text || review.text;
+    await game.save();
+
+    res.json(review);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Failed to update review' });
+  }
+}
+
+async function deleteReview(req, res) {
+  const game = await Game.findById(req.params.gameId);
+  game.reviews = game.reviews.filter(
+    (review) => review._id.toString() !== req.params.reviewId
+  );
+  await game.save();
+  res.sendStatus(204);
+}
--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -1,26 +1,26 @@
-// const express = require('express');
-// const router = express.Router();
-// const reviewsCtrl = require('../controllers/reviews');
-// const ensureLoggedIn = require('../middleware/ensureLoggedIn');
+const express = require('express');
+const router = express.Router();
+const reviewsCtrl = require('../controllers/reviews');
+const ensureLoggedIn = require('../middleware/ensureLoggedIn');
 
-// // All paths start with '/api/reviews'
+// All paths start with '/api/reviews'
 
-// // Protect all defined routes
-// router.use(ensureLoggedIn);
+// Protect all defined routes
+router.use(ensureLoggedIn);
 
-// // GET /api/reviews (INDEX action)
-// router.get('/', reviewsCtrl.index);
+// GET /api/games/:gameId/reviews (INDEX action)
+router.get('/', reviewsCtrl.index);
 
-// //POST /api/reviews (CREATE action)
-// router.post('/', reviewsCtrl.create);
+//POST /api/games/:gameId/reviews (CREATE action)
+router.post('/', reviewsCtrl.create);
 
-// //GET /api/reviews/:reviewId (SHOW action)
-// router.get('/', reviewsCtrl.show);
+//GET /api/games/:gameId/reviews/:reviewId (SHOW action)
+router.get('/:reviewId', reviewsCtrl.show);
 
-// //PUT /api/reviews/:reviewId (UPDATE action)
-// router.put('/', reviewsCtrl.update);
+//PUT /api/reviews/:reviewId (UPDATE action)
+router.put('/:reviewId', reviewsCtrl.update);
 
-// //DELETE /api/reviews/:reviewId (DELETE action)
-// router.delete('/', reviewsCtrl.deleteReview);
+//DELETE /api/reviews/:reviewId (DELETE action)
+router.delete('/:reviewId', reviewsCtrl.deleteReview);
 
-// module.exports = router;
+module.exports = router;
